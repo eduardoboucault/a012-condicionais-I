9a65dc77f8a87e6b5d5d6e9e85a2a8e2e75d73c6
Eduardo Boucault <boucault.labenu@outlook.com>
2022-08-03 23:31:26 -0300

    Exercícios realizados, aguardando feedback.

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,9 +1,47 @@
-let idade;
-let isMaiorIdade;
-let isCursandoOutraFaculdade;
+let idade = Number(prompt('Por favor, digite quantos anos você tem:'))
 
-if(){
+let ensinoMedio = prompt('Você concluiu o ensino médio? (Sim/Não)')
 
+let ensinoMedioB = ensinoMedio
+
+let cursandoFaculdade = prompt('Você está cursando alguma faculdade? (Sim/Não)')
+
+let cursandoFaculdadeB = cursandoFaculdade
+
+//? Transformando string em boolean
+
+if (ensinoMedio.toUpperCase() == 'SIM' || 'SIIM' || 'CIM' || 'SIN'){
+    ensinoMedioB = true //! '=' ATRIBUIR O VALOR SEMPRE QUE PRECISAR ALTERAR A VARIÁVEL LET
+}else if (ensinoMedio.toUpperCase() == 'NÃO' || 'NAO' || 'NAUM' || 'NO'){
+    ensinoMedioB = false //! '=' ATRIBUIR O VALOR SEMPRE QUE PRECISAR ALTERAR A VARIÁVEL LET
 }else{
-	
+    alert('[ERRO]: DADO INVÁLIDO!')
+}
+
+if (cursandoFaculdade.toUpperCase() == 'SIM' || 'SIIM' || 'CIM' || 'SIN'){
+    cursandoFaculdadeB = true //! '=' ATRIBUIR O VALOR SEMPRE QUE PRECISAR ALTERAR A VARIÁVEL LET
+}else if (cursandoFaculdade.toUpperCase() == 'NÃO' || 'NAO' || 'NAUM' || 'NO'){
+    cursandoFaculdadeB = false //! '=' ATRIBUIR O VALOR SEMPRE QUE PRECISAR ALTERAR A VARIÁVEL LET
+}else{
+    alert('[ERRO]: DADO INVÁLIDO!')
+}
+
+//? Aplicando condionais
+
+if (idade >= 18){
+    console.log('A pessoa é maior de idade.')
+}else{
+	console.log('A pessoa é menor de idade')
+}
+
+if (ensinoMedioB === true){
+    console.log('Ensino médio concluído.')
+} else {
+    console.log('Ensino médio não concluído.')
+}
+
+if (cursandoFaculdadeB === true){
+    console.log('Cursando faculdade.')
+} else {
+    console.log('Não está cursando faculdade.')
 }
--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,1 +1,15 @@
-let nacionalidade //= adicione o prompt aqui
+let nacionalidade = prompt('Escreva aqui sua nacionalide:')
+
+if(nacionalidade.toLowerCase == 'brasileira' || 'brasileiro'){
+    console.log(`Você é: ${nacionalidade}!` )
+}else if(nacionalidade.toLowerCase == 'argentina' || 'argentino'){
+    console.log(`Você é: ${nacionalidade}!` )
+}else if(nacionalidade.toLowerCase == 'uruguaia' || 'uruguaio'){
+    console.log(`Você é: ${nacionalidade}!` )
+}else if(nacionalidade.toLowerCase == 'chilena' || 'chileno'){
+    console.log(`Você é: ${nacionalidade}!` )
+}else if(nacionalidade.toLowerCase == 'colombiana' || 'colombiano'){
+    console.log(`Você é: ${nacionalidade}!` )
+}else{
+    console.log(`Nacionalidade não encontrada.` )
+}
